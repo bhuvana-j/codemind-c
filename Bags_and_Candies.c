269a1674da051f5493ae261b5f3cc1bcd1da7fc4
Bhuvana-j <119840560+bhuvana-j@users.noreply.github.com>
2022-12-28 11:17:45 +0530

    #include<stdio.h> int main() {     int n,k,m,km;     scanf("%d%d%d",&n,&k,&m);     km=k*m;     if(n%km==0)     {         printf("%d",n/km);     }     else     printf("%d",n/km+1); }

---
+++ Bags_and_Candies.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,m,km;
+    scanf("%d%d%d",&n,&k,&m);
+    km=k*m;
+    if(n%km==0)
+    {
+        printf("%d",n/km);
+    }
+    else
+    printf("%d",n/km+1);
+}
